8c9276974656558fbfc6e5c056c62989f88978d8
Syed Muneeb <syedmuneeb54@gmail.com>
2023-01-12 00:11:59 +0500

    main to incorp pipeline

---
+++ main.py
@@ -0,0 +1,15 @@
+from utils import get_products_data, argument_parser, scrap_data
+
+# import logging
+
+
+def main():
+
+    walmart_site = "https://www.bestbuy.ca/en-ca/category/ps5-consoles/17583383"
+    args = argument_parser()
+    products_dict = scrap_data(walmart_site)
+    scrapped_data = get_products_data(products_dict)
+
+
+if __name__ == "__main__":
+    main()
