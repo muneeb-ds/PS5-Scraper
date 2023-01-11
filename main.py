4340488c827df67d14fd9c641adfd1bc1e014866
Syed Muneeb <syedmuneeb54@gmail.com>
2023-01-12 01:18:25 +0500

    webscrap to oop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.utils import get_products_data, argument_parser, scrap_data
+from src.scrap import WebScrap
+from src.utils import get_products_data, argument_parser
 
 # import logging
 
@@ -7,7 +8,7 @@ def main():
 
     walmart_site = "https://www.bestbuy.ca/en-ca/category/ps5-consoles/17583383"
     args = argument_parser()
-    products_dict = scrap_data(walmart_site)
+    products_dict = WebScrap(walmart_site).run()
     scrapped_data = get_products_data(products_dict)
 
 
--- src/scrap.py
+++ src/scrap.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -25,7 +25,11 @@ class WebScrap:
             service=Service(driver_path), options=chrome_options
         )
 
+        self.product_info = {}
+        
         self.driver.get(web_url)
+
+
 
     def wait_until_element_exist(self, locator, loc_text):
         try:
@@ -47,3 +51,85 @@ class WebScrap:
             return self.driver.find_elements(locator, loc_text)
         elif method == "first":
             return self.driver.find_element(locator, loc_text)
+
+    def home_page(self):
+        self.wait_until_element_exist(By.CLASS_NAME, "productList_31W-E")
+
+        product_container = self.get_element(By.CLASS_NAME, "productList_31W-E", method = "first")
+        links = product_container.find_elements(By.CLASS_NAME, "link_3hcyN")
+        links = [link.get_attribute("href") for link in links]
+
+        return links
+
+    def parse_link(self, link):
+        self.driver.get(link)
+
+        self.wait_until_element_exist(By.CLASS_NAME, "productName_2KoPa")
+        product_name = self.get_element(By.CLASS_NAME, "productName_2KoPa", method = "first").text
+        button1 = self.check_exists(By.LINK_TEXT, "Check other stores")
+        if not button1:
+            return False
+
+        button1.click()
+
+        self.product_info[product_name] = {}
+        self.product_info[product_name]['link']  = link
+
+        return product_name
+
+    def input_postal_code(self, post_code="R2M"):
+        self.wait_until_element_exist(By.ID, "postalCode")
+
+        postal_code = self.get_element(By.ID, "postalCode", method = 'first')
+        postal_code.send_keys(post_code)
+        button_xpath = "//*[@id='root']/div/div[3]/div/div/div/div[3]/div[2]/div/div/button[1]"
+        self.wait_until_element_exist(By.XPATH,button_xpath)
+        self.get_element(
+            By.XPATH,
+            button_xpath
+        ).click()
+
+    def get_store_lists(self):
+        self.wait_until_element_exist(By.CLASS_NAME, "storeListItem_3piwR")
+
+        button_see_more = (
+            "//*[@id='root']/div/div[3]/div/div/div/div[4]/div/div[2]/div/button"
+        )
+        while self.check_exists(By.XPATH, button_see_more):
+            self.get_element(By.XPATH, button_see_more).click()
+
+        store_lists = self.get_element(By.CLASS_NAME, "storeListItem_3piwR", method = 'all')
+
+        return store_lists
+
+    def availability(self, store):
+        self.wait_until_element_exist(By.CLASS_NAME, "availabilityMessage_1waQP")
+        store_name = store.find_element(By.CLASS_NAME, "name_1zPVg").text
+        self.driver.get_screenshot_as_file("screenshot.png")
+        status = store.find_element(
+            By.CLASS_NAME, "availabilityMessage_1waQP"
+        ).text
+
+        return store_name,status
+
+    def run(self):
+        links = self.home_page()
+        for link in links:
+            product_ = self.parse_link(link)
+
+            if not product_:
+                continue
+
+            self.input_postal_code()
+            self.product_info[product_]['price'] = self.driver.find_element(
+                By.XPATH,
+                "//*[@id='root']/div/div[3]/div/div/div/div[2]/a/div/div/div[2]/div[3]/span/div/div",
+                ).text
+            stores = self.get_store_lists()
+
+            for store in stores:
+                store_name, status = self.availability(store)
+                self.product_info[product_][store_name] = status
+        self.driver.quit()
+
+        return self.product_info
